0b6f37aac4215d7571b4c0ca472157d2cf2fc134
Shadow777885 <olivierblondin777885@gmail.com>
2022-09-22 15:27:01 -0400

    Update test.php

--- Lab 1/test.php
+++ Lab 1/test.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <body>
-<p> Your Username is: <?=htmlspecialchars($_POST['fname'])?> </p> 
+<p> Your Username is: <?=htmlspecialchars($_POST['fname'])?> 
 <p> Your Password is: <?=htmlspecialchars($_POST['password'])?> 
 <br>LMAO what a terrible password </p>
 </ body>
