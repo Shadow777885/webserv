602714106ac0777c7689e923afb2bab7464c4d45
Shadow777885 <olivierblondin777885@gmail.com>
2022-10-20 13:56:24 -0400

    Test1 post to db

---
+++ Lab 1/send.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <p>
+    <?php
+    $servername = "localhost";
+    $username = "php";
+    $password = "1.1.";
+    $dbname = "Users";
+    $conn = mysqli_connect($server, $username, $password, $database);
+    $newuser = $_POST['fname'];
+    $newid = $_POST['ID'];
+
+    $sql = "INSERT INTO Userinfo (username, userid) VALUES ($newuser, $newid)";
+    $result = mysqli_query($conn, $sql);
+    mysqli_close($conn);
+    ?>
+</body>
+</html>
