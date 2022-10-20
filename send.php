cf05aedd20184072f5d77b8a11c159d6eca27993
Shadow777885 <olivierblondin777885@gmail.com>
2022-10-20 15:24:07 -0400

    Update send.php

--- send.php
+++ send.php
@@ -16,12 +16,13 @@
     $sql = "INSERT INTO Userinfo (username, userid) VALUES ('{$newuser}', {$newid});";
     $result = mysqli_query($conn, $sql);
     echo $result ? "Sent properly." : "Failure: {mysqli_error($conn)}"; 
-    $sql = "SELECT * FROM Userinfo WHERE id='{$pullID}';";
+    $sql = "SELECT id FROM Userinfo WHERE id='{$pullID}';";
     $result = mysqli_query($conn, $sql);
-    foreach($result as $username){ 
-        echo "id:{$username["id"]} | Username: {$username["username"]} {$username["userid"]}\n";
-    }
      mysqli_close($conn);
+     $row = mysql_fetch_row($result);
+     echo $row[0]; // id
+     echo $row[1]; // Username
+     echo $row[2]; // Userid
     ?>
     <p>
 </body>
