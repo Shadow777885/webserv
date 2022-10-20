3da24cae74919765c64d843eb18b2908a202568b
Shadow777885 <olivierblondin777885@gmail.com>
2022-10-20 14:23:37 -0400

    Update send.php

--- send.php
+++ send.php
@@ -8,8 +8,8 @@
     $password = "1.1.";
     $dbname = "Users";
     $conn = mysqli_connect($server, $username, $password, $database);
-    $newuser = =htmlspecialchars($_POST['fname']);
-    $newid =htmlspecialchars($_POST['ID']);
+    $newuser = htmlspecialchars($_POST['fname']);
+    $newid = htmlspecialchars($_POST['ID']);
 
     $sql = "INSERT INTO Userinfo (username, userid) VALUES ($newuser, $newid);";
     $result = mysqli_query($conn, $sql);
