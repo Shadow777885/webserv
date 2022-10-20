b2f77818790e6eb5549e7b672096529054fecbe1
Shadow777885 <olivierblondin777885@gmail.com>
2022-10-20 14:22:33 -0400

    updated

--- test.php
+++ test.php
@@ -5,5 +5,5 @@
 <p> Your Username is: <?=htmlspecialchars($_POST['fname'])?> 
 <p> Your Password is: <?=htmlspecialchars($_POST['password'])?> 
 <br>LMAO what a terrible password </p>
-</ body>
+</body>
 </html>
