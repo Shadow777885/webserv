7f4f4d204b715c5378c93e6717097e8786cf2ee2
Shadow777885 <olivierblondin777885@gmail.com>
2022-10-20 14:42:35 -0400

    Update send.php

--- send.php
+++ send.php
@@ -11,7 +11,7 @@
     $newuser = htmlspecialchars($_POST['fname']);
     $newid = htmlspecialchars($_POST['ID']);
 
-    $sql = "INSERT INTO Userinfo (username, userid) VALUES ($newuser, $newid);";
+    $sql = "INSERT INTO Userinfo (username, userid) VALUES ('{$newuser}', '{$newid}');";
     echo $sql;
     $result = mysqli_query($conn, $sql);
     echo $result ? "Sent properly." : "Failure: {mysqli_error($conn)}"; 
