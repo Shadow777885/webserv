c7ec9aa0a741c807793eecd8bbcf88c51010d322
Shadow777885 <olivierblondin777885@gmail.com>
2022-10-27 20:59:30 -0400

    properly structured repo

--- send.php
+++ send.php
@@ -16,7 +16,7 @@
     $result = mysqli_query($conn, $sql);
     //$sql = "INSERT INTO Userinfo (username, userid) VALUES ('{$newuser}', {$newid});";
     //echo $result ? "Sent properly." : "Failure: {mysqli_error($conn)}"; 
-    $sql = "SELECT id FROM Userinfo WHERE id={$pullID};";
+    $sql = "SELECT id, username, userid FROM Userinfo WHERE id={$pullID};";
     $result = mysqli_query($conn, $sql);
     foreach($result as $row) // There should only be one row returned! 
     { 
