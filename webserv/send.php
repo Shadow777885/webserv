0e888c9c9fa8a3e6f77ade03a800f9bf68c9cb27
Shadow777885 <olivierblondin777885@gmail.com>
2022-10-27 20:57:07 -0400

    Create send.php

---
+++ webserv/send.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <link rel= "stylesheet" href="style.css">
+    <p>
+    <?php
+    $server = "localhost";
+    $username = "php";
+    $password = "1.1.";
+    $database = "Users";
+    $conn = mysqli_connect($server, $username, $password, $database);
+    $newuser = htmlspecialchars($_POST['fname']);
+    $newid = htmlspecialchars($_POST['ID']);
+    $pullID = htmlspecialchars($_POST['pullID']);
+
+    $result = mysqli_query($conn, $sql);
+    //$sql = "INSERT INTO Userinfo (username, userid) VALUES ('{$newuser}', {$newid});";
+    //echo $result ? "Sent properly." : "Failure: {mysqli_error($conn)}"; 
+    $sql = "SELECT id, username, userid FROM Userinfo WHERE id={$pullID};";
+    foreach($result as $row) // There should only be one row returned! 
+    { 
+        echo "ID: {$row['id']} | {$row['username']} | UserID: {$row['userid']}"; 
+    }
+    mysqli_close($conn);
+    ?>
+    <p>
+</body>
+</html>
